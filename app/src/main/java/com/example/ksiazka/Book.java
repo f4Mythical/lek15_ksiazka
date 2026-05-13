361baea1ab13111fb0c180befff7db6f753946e5
f4Mythical <grynka.adam@gmail.com>
2026-05-13 19:00:40 +0200

    add constructor to  book class

--- app/src/main/java/com/example/ksiazka/Book.java
+++ app/src/main/java/com/example/ksiazka/Book.java
@@ -1,4 +1,36 @@
 package com.example.ksiazka;
 
 public class Book {
+    private String tytul;
+    private String autor;
+    private String gatunek;
+    private boolean czNowa;
+    private double cena;
+    private int promocja;
+    private String dostepnosc;
+    private String kategoriaWiekowa;
+    private float ocena;
+
+    public Book(String tytul, String autor, String gatunek, boolean czNowa,
+                double cena, int promocja, String dostepnosc, String kategoriaWiekowa, float ocena) {
+        this.tytul = tytul;
+        this.autor = autor;
+        this.gatunek = gatunek;
+        this.czNowa = czNowa;
+        this.cena = cena;
+        this.promocja = promocja;
+        this.dostepnosc = dostepnosc;
+        this.kategoriaWiekowa = kategoriaWiekowa;
+        this.ocena = ocena;
+    }
+
+    public String getTytul() { return tytul; }
+    public String getAutor() { return autor; }
+    public String getGatunek() { return gatunek; }
+    public boolean isCzNowa() { return czNowa; }
+    public double getCena() { return cena; }
+    public int getPromocja() { return promocja; }
+    public String getDostepnosc() { return dostepnosc; }
+    public String getKategoriaWiekowa() { return kategoriaWiekowa; }
+    public float getOcena() { return ocena; }
 }
